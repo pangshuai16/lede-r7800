15e6d39bc66242c647e7eb04c7cc8ce4518782c2
pangshuai16 <101418515+pangshuai16@users.noreply.github.com>
2023-07-13 00:14:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,9 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+# istore应用商店
+sed -i '$src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+
 # passwall独立包
 sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
 sed -i '$a src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default  //如果是编译passwall2着不需要否则会加载重复软件包
